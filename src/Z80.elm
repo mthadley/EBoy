ae18892fedd152c13dfebfd05376f0703da60807
Michael Hadley <michael.hadley@liferay.com>
2017-01-29 21:11:57 -0800

    Last of the first bytespace of instructions

--- src/Z80.elm
+++ src/Z80.elm
@@ -1,6 +1,8 @@
 module Z80 exposing (..)
 
 {-| An emulation of a Z80 CPU core
+    TODO: Possible remove `ByteData` and `WordData`, these might be
+    meaningless.
 -}
 
 
@@ -51,6 +53,7 @@ type WordRegister
     | BC
     | HL
     | DE
+    | AF
 
 
 {-| ByteData
@@ -79,6 +82,7 @@ type WordData
 type LoadByteTarget
     = IntoByteRegister ByteRegister
     | IntoMem WordRegister
+    | IntoMemWordData
 
 
 {-| Sources for 8-bit `LD` instructions.
@@ -86,7 +90,8 @@ type LoadByteTarget
 type LoadByteSource
     = FromByteRegister ByteRegister
     | FromMem WordRegister
-    | ByteData ByteData
+    | FromMemWordData
+    | FromByteData ByteData
 
 
 {-| Targets for 16-bit `LD` instructions.
@@ -101,7 +106,31 @@ type LoadWordTarget
 type LoadWordSource
     = SPPlusR8
     | FromWordRegister WordRegister
-    | WordData WordData
+    | FromWordData WordData
+    | FromSPByteData ByteData
+
+
+{-| Sources for `LDH` instructions.
+-}
+type LoadOffsetSource
+    = FromRegisterA
+    | FromMemDataOffset
+    | FromMemCOffset
+
+
+{-| Targets for `LDH` instructions.
+-}
+type LoadOffsetTarget
+    = IntoRegisterA
+    | IntoMemDataOffset
+    | IntoMemCOffset
+
+
+{-| Jump Targets
+-}
+type JumpTarget
+    = JumpData WordData
+    | JumpMemHL
 
 
 {-| Param for arithmetic operations (`ADD`, `SUB`, etc.).
@@ -109,6 +138,7 @@ type LoadWordSource
 type Param
     = WithRegister ByteRegister
     | WithMemHL
+    | WithData ByteData
 
 
 {-| Flags checked by jump instructions.
@@ -126,10 +156,21 @@ type FlagCondition
     | NoCondition
 
 
+{-| A union type representing all of the supported base set of instructions
+    of the Z80. There are a few extra "synthetic" instructions added to make
+    it easier to work with 16-bit (Word) operatoins. These include:
+    * `ADDW`
+    * `INCW`
+    * `LDW`
+    * `DECW`
+    * `ADDSP`
+-}
 type Op
     = INVALID Int
+    | NONE
     | NOP
     | LD LoadByteTarget LoadByteSource
+    | LDH LoadOffsetTarget LoadOffsetSource
     | LDW LoadWordTarget LoadWordSource
     | INC Param
     | INCW WordRegister
@@ -137,7 +178,9 @@ type Op
     | DECW WordRegister
     | RLCA
     | ADD ByteRegister Param
+    | ADC ByteRegister Param
     | ADDW WordRegister WordRegister
+    | ADDSP
     | RRCA
     | STOP
     | RLA
@@ -150,6 +193,22 @@ type Op
     | SCF
     | CCF
     | HALT
+    | SUB Param
+    | SBC Param
+    | AND Param
+    | XOR Param
+    | OR Param
+    | CP Param
+    | RET FlagCondition
+    | POP WordRegister
+    | JP FlagCondition JumpTarget
+    | CALL FlagCondition
+    | PUSH WordRegister
+    | RST Int
+    | PREFIX_CB
+    | RETI
+    | DI
+    | EI
 
 
 {-| Decodes an instruction, returning a tuple of the `Op` and
@@ -159,7 +218,7 @@ decode : Int -> ( Op, Cycles )
 decode code =
     case code of
         0x01 ->
-            LDW (IntoWordRegister BC) (WordData D16) @ 12
+            LDW (IntoWordRegister BC) (FromWordData D16) @ 12
 
         0x02 ->
             LD (IntoMem BC) (FromByteRegister A) @ 8
@@ -174,7 +233,7 @@ decode code =
             DEC (WithRegister B) @ 4
 
         0x06 ->
-            LD (IntoByteRegister B) (ByteData D8) @ 8
+            LD (IntoByteRegister B) (FromByteData D8) @ 8
 
         0x07 ->
             RLCA @ 4
@@ -198,7 +257,7 @@ decode code =
             DEC (WithRegister C) @ 4
 
         0x0E ->
-            LD (IntoByteRegister C) (ByteData D8) @ 8
+            LD (IntoByteRegister C) (FromByteData D8) @ 8
 
         0x0F ->
             RRCA @ 4
@@ -207,7 +266,7 @@ decode code =
             STOP @ 4
 
         0x11 ->
-            LDW (IntoWordRegister DE) (WordData D16) @ 12
+            LDW (IntoWordRegister DE) (FromWordData D16) @ 12
 
         0x12 ->
             LD (IntoMem DE) (FromByteRegister A) @ 8
@@ -222,7 +281,7 @@ decode code =
             DEC (WithRegister D) @ 4
 
         0x16 ->
-            LD (IntoByteRegister D) (ByteData D8) @ 8
+            LD (IntoByteRegister D) (FromByteData D8) @ 8
 
         0x17 ->
             RLA @ 4
@@ -246,7 +305,7 @@ decode code =
             DEC (WithRegister E) @ 4
 
         0x1E ->
-            LD (IntoByteRegister E) (ByteData D8) @ 8
+            LD (IntoByteRegister E) (FromByteData D8) @ 8
 
         0x1F ->
             RRA @ 4
@@ -255,7 +314,7 @@ decode code =
             JR (NotSet Zero) / ( 12, 8 )
 
         0x21 ->
-            LDW (IntoWordRegister HL) (WordData D16) @ 12
+            LDW (IntoWordRegister HL) (FromWordData D16) @ 12
 
         0x22 ->
             LDI (IntoMem HL) (FromByteRegister A) @ 8
@@ -270,7 +329,7 @@ decode code =
             DEC (WithRegister H) @ 4
 
         0x26 ->
-            LD (IntoByteRegister H) (ByteData D8) @ 8
+            LD (IntoByteRegister H) (FromByteData D8) @ 8
 
         0x27 ->
             DAA @ 4
@@ -294,7 +353,7 @@ decode code =
             DEC (WithRegister L) @ 4
 
         0x2E ->
-            LD (IntoByteRegister L) (ByteData D8) @ 8
+            LD (IntoByteRegister L) (FromByteData D8) @ 8
 
         0x2F ->
             CPL @ 4
@@ -303,7 +362,7 @@ decode code =
             JR (NotSet Carry) / ( 12, 8 )
 
         0x31 ->
-            LDW (IntoWordRegister SP) (WordData D16) @ 12
+            LDW (IntoWordRegister SP) (FromWordData D16) @ 12
 
         0x32 ->
             LDD (IntoMem HL) (FromByteRegister A) @ 8
@@ -318,7 +377,7 @@ decode code =
             DEC WithMemHL @ 12
 
         0x36 ->
-            LD (IntoMem HL) (ByteData D8) @ 12
+            LD (IntoMem HL) (FromByteData D8) @ 12
 
         0x37 ->
             SCF @ 4
@@ -342,7 +401,7 @@ decode code =
             DEC (WithRegister A) @ 4
 
         0x3E ->
-            LD (IntoByteRegister A) (ByteData D8) @ 8
+            LD (IntoByteRegister A) (FromByteData D8) @ 8
 
         0x3F ->
             CCF @ 4
@@ -542,12 +601,398 @@ decode code =
         0x80 ->
             ADD A (WithRegister B) @ 4
 
-        val ->
-            INVALID val @ 0
+        0x81 ->
+            ADD A (WithRegister C) @ 4
+
+        0x82 ->
+            ADD A (WithRegister D) @ 4
+
+        0x83 ->
+            ADD A (WithRegister E) @ 4
+
+        0x84 ->
+            ADD A (WithRegister H) @ 4
+
+        0x85 ->
+            ADD A (WithRegister L) @ 4
+
+        0x86 ->
+            ADD A WithMemHL @ 8
+
+        0x87 ->
+            ADD A (WithRegister A) @ 4
+
+        0x88 ->
+            ADC A (WithRegister B) @ 4
+
+        0x89 ->
+            ADC A (WithRegister C) @ 4
+
+        0x8A ->
+            ADC A (WithRegister D) @ 4
+
+        0x8B ->
+            ADC A (WithRegister E) @ 4
+
+        0x8C ->
+            ADC A (WithRegister H) @ 4
+
+        0x8D ->
+            ADC A (WithRegister L) @ 4
+
+        0x8E ->
+            ADC A WithMemHL @ 8
+
+        0x8F ->
+            ADC A (WithRegister A) @ 4
+
+        0x90 ->
+            SUB (WithRegister B) @ 4
+
+        0x91 ->
+            SUB (WithRegister C) @ 4
+
+        0x92 ->
+            SUB (WithRegister D) @ 4
+
+        0x93 ->
+            SUB (WithRegister E) @ 4
+
+        0x94 ->
+            SUB (WithRegister H) @ 4
+
+        0x95 ->
+            SUB (WithRegister L) @ 4
+
+        0x96 ->
+            SUB WithMemHL @ 8
+
+        0x97 ->
+            SUB (WithRegister A) @ 4
+
+        0x98 ->
+            SBC (WithRegister B) @ 4
+
+        0x99 ->
+            SBC (WithRegister C) @ 4
+
+        0x9A ->
+            SBC (WithRegister D) @ 4
+
+        0x9B ->
+            SBC (WithRegister E) @ 4
+
+        0x9C ->
+            SBC (WithRegister H) @ 4
+
+        0x9D ->
+            SBC (WithRegister L) @ 4
+
+        0x9E ->
+            SBC WithMemHL @ 8
+
+        0x9F ->
+            SBC (WithRegister A) @ 4
+
+        0xA0 ->
+            AND (WithRegister B) @ 4
+
+        0xA1 ->
+            AND (WithRegister C) @ 4
+
+        0xA2 ->
+            AND (WithRegister D) @ 4
+
+        0xA3 ->
+            AND (WithRegister E) @ 4
+
+        0xA4 ->
+            AND (WithRegister H) @ 4
+
+        0xA5 ->
+            AND (WithRegister L) @ 4
+
+        0xA6 ->
+            AND WithMemHL @ 8
+
+        0xA7 ->
+            AND (WithRegister A) @ 4
+
+        0xA8 ->
+            XOR (WithRegister B) @ 4
+
+        0xA9 ->
+            XOR (WithRegister C) @ 4
+
+        0xAA ->
+            XOR (WithRegister D) @ 4
+
+        0xAB ->
+            XOR (WithRegister E) @ 4
+
+        0xAC ->
+            XOR (WithRegister H) @ 4
+
+        0xAD ->
+            XOR (WithRegister L) @ 4
+
+        0xAE ->
+            XOR WithMemHL @ 8
+
+        0xAF ->
+            XOR (WithRegister A) @ 4
+
+        0xB0 ->
+            OR (WithRegister B) @ 4
+
+        0xB1 ->
+            OR (WithRegister C) @ 4
+
+        0xB2 ->
+            OR (WithRegister D) @ 4
+
+        0xB3 ->
+            OR (WithRegister E) @ 4
+
+        0xB4 ->
+            OR (WithRegister H) @ 4
+
+        0xB5 ->
+            OR (WithRegister L) @ 4
+
+        0xB6 ->
+            OR WithMemHL @ 8
+
+        0xB7 ->
+            OR (WithRegister A) @ 4
+
+        0xB8 ->
+            CP (WithRegister B) @ 4
+
+        0xB9 ->
+            CP (WithRegister C) @ 4
+
+        0xBA ->
+            CP (WithRegister D) @ 4
+
+        0xBB ->
+            CP (WithRegister E) @ 4
+
+        0xBC ->
+            CP (WithRegister H) @ 4
+
+        0xBD ->
+            CP (WithRegister L) @ 4
+
+        0xBE ->
+            CP WithMemHL @ 8
+
+        0xBF ->
+            CP (WithRegister A) @ 4
+
+        0xC0 ->
+            RET (NotSet Zero) / ( 20, 8 )
+
+        0xC1 ->
+            POP BC @ 12
+
+        0xC2 ->
+            JP (NotSet Zero) (JumpData A16) / ( 16, 12 )
+
+        0xC3 ->
+            JP NoCondition (JumpData A16) / ( 16, 12 )
+
+        0xC4 ->
+            CALL (NotSet Zero) / ( 24, 12 )
+
+        0xC5 ->
+            PUSH BC @ 16
+
+        0xC6 ->
+            ADD A (WithData D8) @ 8
+
+        0xC7 ->
+            RST 0x00 @ 16
+
+        0xC8 ->
+            RET (Set Zero) / ( 20, 8 )
+
+        0xC9 ->
+            RET NoCondition @ 16
+
+        0xCA ->
+            JP (Set Zero) (JumpData A16) / ( 16, 12 )
+
+        0xCB ->
+            PREFIX_CB @ 4
+
+        0xCC ->
+            CALL (Set Zero) / ( 24, 12 )
+
+        0xCD ->
+            CALL NoCondition @ 24
+
+        0xCE ->
+            ADC A (WithData D8) @ 8
+
+        0xCF ->
+            RST 0x08 @ 16
+
+        0xD0 ->
+            RET (NotSet Carry) / ( 20, 8 )
+
+        0xD1 ->
+            POP DE @ 12
+
+        0xD2 ->
+            JP (NotSet Carry) (JumpData A16) / ( 16, 12 )
+
+        0xD3 ->
+            none
+
+        0xD4 ->
+            CALL (NotSet Carry) / ( 24, 12 )
+
+        0xD5 ->
+            PUSH DE @ 16
+
+        0xD6 ->
+            SUB (WithData D8) @ 8
+
+        0xD7 ->
+            RST 0x10 @ 16
+
+        0xD8 ->
+            RET (Set Carry) / ( 20, 8 )
+
+        0xD9 ->
+            RETI @ 16
+
+        0xDA ->
+            JP (Set Carry) (JumpData A16) / ( 16, 12 )
+
+        0xDB ->
+            none
+
+        0xDC ->
+            CALL (Set Carry) / ( 24, 12 )
+
+        0xDD ->
+            none
+
+        0xDE ->
+            SBC (WithRegister A) @ 8
+
+        0xDF ->
+            RST 0x18 @ 16
+
+        0xE0 ->
+            LDH IntoMemDataOffset FromRegisterA @ 12
+
+        0xE1 ->
+            POP HL @ 12
+
+        0xE2 ->
+            LDH IntoMemCOffset FromRegisterA @ 8
+
+        0xE3 ->
+            none
+
+        0xE4 ->
+            none
+
+        0xE5 ->
+            PUSH HL @ 16
+
+        0xE6 ->
+            AND (WithData D8) @ 8
+
+        0xE7 ->
+            RST 0x20 @ 16
+
+        0xE8 ->
+            ADDSP @ 16
+
+        0xE9 ->
+            JP NoCondition JumpMemHL @ 4
+
+        0xEA ->
+            LD IntoMemWordData (FromByteRegister A) @ 16
+
+        0xEB ->
+            none
+
+        0xEC ->
+            none
+
+        0xED ->
+            none
+
+        0xEE ->
+            XOR (WithData D8) @ 8
+
+        0xEF ->
+            RST 0x28 @ 16
+
+        0xF0 ->
+            LDH IntoRegisterA FromMemDataOffset @ 12
+
+        0xF1 ->
+            POP AF @ 12
+
+        0xF2 ->
+            LDH IntoRegisterA FromMemCOffset @ 8
+
+        0xF3 ->
+            DI @ 4
+
+        0xF4 ->
+            none
+
+        0xF5 ->
+            PUSH AF @ 16
+
+        0xF6 ->
+            OR (WithData D8) @ 8
+
+        0xF7 ->
+            RST 0x30 @ 16
+
+        0xF8 ->
+            LDW (IntoWordRegister HL) (FromSPByteData R8) @ 12
+
+        0xF9 ->
+            LDW (IntoWordRegister SP) (FromWordRegister HL) @ 8
+
+        0xFA ->
+            LD IntoMemWordData (FromByteRegister A) @ 16
+
+        0xFB ->
+            EI @ 4
+
+        0xFC ->
+            none
+
+        0xFD ->
+            none
+
+        0xFE ->
+            CP (WithData D8) @ 8
+
+        0xFF ->
+            RST 0x38 @ 16
+
+        b ->
+            INVALID b @ 0
+
+
+none : ( Op, Cycles )
+none =
+    NONE @ 0
 
 
 {-| Represents how many cycles an intruction will take. Some
-    differ since they are branching (`JR`, `JRNZ`, etc.).
+    have two values since they are branching (`JR`, `JRNZ`, etc.).
 -}
 type Cycles
     = Always Int
